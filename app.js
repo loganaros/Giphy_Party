d10c666f182d69a4f930821cf1c7897ffa412de2
loganaros <logabeast11@gmail.com>
2024-01-10 17:43:42 -0700

    final version

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Let's get this party started!");
+async function getGIF() {
+    let searchVal = document.getElementById("searchInput").value;
+    let res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchVal}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    let index = Math.floor(Math.random() * res.data.data.length);
+    return res.data.data[index].images.original.url;
+}
+
+$("form").on("submit", async (e) => {
+    e.preventDefault();
+    let gif = document.createElement("img");
+    gif.src = await getGIF();
+    $("#gifArea").append(gif);
+});
+
+$("#deleteAll").on("click", (e) => {
+    e.preventDefault();
+    $("#gifArea").empty();
+})
